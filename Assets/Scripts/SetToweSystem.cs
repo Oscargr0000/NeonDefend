109012f996cc1b1e05810050f7d25c26dfce0953
Oscargr0000 <oscargr0000@gmail.com>
2023-02-03 00:27:35 +0100

    asdasdasd

--- Assets/Scripts/SetToweSystem.cs
+++ Assets/Scripts/SetToweSystem.cs
@@ -1,21 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.UI;
+using UnityEngine.UI;
 using TMPro;
 
 public class SetToweSystem : MonoBehaviour
 {
     public Canvas towersCan;
-    // Start is called before the first frame update
-    void Start()
+    public TextMeshProUGUI editModeText;
+
+    public Button[] towersButton;
+    public GameObject[] towersObjets;
+    private GameObject selectedTower;
+    private GameManager _gm;
+
+    private bool settingMode;
+
+    public int[] priceList;
+    private int towerPrice;
+
+    private void Start()
     {
-        
+        _gm = FindObjectOfType<GameManager>();
+    }
+    private void Update()
+    {
+        if (settingMode && Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            SetTower();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void SetTower()
     {
-        
+        //Acede a la posicion del mouse cuando hace click
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 objectPos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, Camera.main.nearClipPlane));
+        Vector2 objectPos2D = new Vector2(objectPos.x, objectPos.y);
+
+
+
+        //Colocar la torre en la posicion del raton
+        Instantiate(selectedTower, objectPos2D, Quaternion.identity);
+
+        //Resta del precio a los puntos
+        _gm.points = _gm.points -= towerPrice;
+
+
+        //Quita todo lo relacionado con la seleccion
+        editModeText.text = "OFF";
+        selectedTower = null;
+        towerPrice = 0;
+        settingMode = false;
+    }
+
+
+    public void SelectMode(int towerToSelect)
+    {
+        if(_gm.points >= priceList[towerToSelect])
+        {
+            settingMode = true;
+
+            //Accede a la torre que pretende colocar
+            selectedTower = towersObjets[towerToSelect];
+
+            towerPrice = priceList[towerToSelect];
+
+            EditMode();
+        }
+    }
+
+    void EditMode()
+    {
+        editModeText.text = "ON";
     }
 }
